c0228e79b45212706bd58757267071663fea189a
John Rizzo <johnrizzo1@gmail.com>
2024-11-24 16:44:08 +0000

    migrating setup to pyproject approach with fh flake setup

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "ADA Another Digital Assistant";
+
+  inputs = {
+    dream2nix.url = "github:nix-community/dream2nix";
+    nixpkgs.follows = "dream2nix/nixpkgs";
+  };
+
+  outputs = {
+    self,
+    dream2nix,
+    nixpkgs,
+  }: let
+    eachSystem = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+  in {
+    packages = eachSystem (system: {
+      default = dream2nix.lib.evalModules {
+        packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
+        modules = [
+          ./default.nix
+          {
+            paths.projectRoot = ./.;
+            paths.projectRootFile = "flake.nix";
+            paths.package = ./.;
+          }
+        ];
+      };
+    });
+    devShells = eachSystem (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        ada = self.packages.${system}.default;
+      in {
+        default = pkgs.mkShell {
+          inputsFrom = [ada.devShell];
+          packages = [
+            pkgs.portaudio
+            pkgs.tbb
+            pkgs.ada
+          ];
+        };
+      });
+  };
+}
