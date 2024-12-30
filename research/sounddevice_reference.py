e2cad5167051de843124fbc78efada5d968e4a69
John Rizzo <johnrizzo1@gmail.com>
2024-12-29 23:29:52 -0500

    Converted async methods to class for organization

--- research/sounddevice_reference.py
+++ research/sounddevice_reference.py
@@ -17,77 +17,84 @@ import sounddevice as sd
 
 from datetime import datetime, timedelta
 
+import logging
 
-async def stream_generator(blocksize, *, channels=1, dtype='float32',
-                           pre_fill_blocks=10, **kwargs):
-  """
-  Generator that yields blocks of input/output data as NumPy arrays.
-
-  The output blocks are uninitialized and have to be filled with
-  appropriate audio signals.
-  """
-  assert blocksize != 0
-  q_in = asyncio.Queue()
-  q_out = queue.Queue()
-  loop = asyncio.get_event_loop()
-
-  def callback(indata, outdata, frame_count, time_info, status):
-    loop.call_soon_threadsafe(q_in.put_nowait, (indata.copy(), status))
-    outdata[:] = q_out.get_nowait()
-
-  # pre-fill output queue
-  for _ in range(pre_fill_blocks):
-    q_out.put(np.zeros((blocksize, channels), dtype=dtype))
-
-  stream = sd.Stream(blocksize=blocksize, callback=callback, dtype=dtype,
-                     channels=channels, **kwargs)
-  with stream:
-    while True:
-      indata, status = await q_in.get()
-      outdata = np.empty((blocksize, channels), dtype=dtype)
-      yield indata, outdata, status
-      q_out.put_nowait(outdata)
+logging.basicConfig(level=logging.INFO)
 
 
-async def wire_coro(**kwargs):
-  """
-  Create a connection between audio inputs and outputs.
+class LocalStreamingAda:
+  def __init__(self, **kwargs):
+    self.kwargs = kwargs
+    self._stream = None
+    self.logger = logging.getLogger(__name__)
 
-  Asynchronously iterates over a stream generator and for each block
-  simply copies the input data into the output block.
+  async def stream_generator(self, blocksize, *, channels=1, dtype='float32',
+                            pre_fill_blocks=10, **kwargs):
+    """
+    Generator that yields blocks of input/output data as NumPy arrays.
 
-  """
-  conversation_status = 'Not Started'
-  last_started_set_time = datetime.now()
-  async for indata, outdata, status in stream_generator(**kwargs):
-    if status:
-      print(status)
-    if conversation_status == 'Not Started' and indata.max() >= 0.1:
-      print(f"Since Conversation Started: {datetime.now() - last_started_set_time}")
-      if (datetime.now() - last_started_set_time) > timedelta(seconds=0.005):
-        conversation_status = 'Started'
-      last_started_set_time = datetime.now()
-    elif conversation_status == 'Started' and indata.max() < 0.1:
-      if (datetime.now() - last_started_set_time) > timedelta(seconds=0.05):
-        conversation_status = 'Not Started'
-    print('min:', indata.min(), '\t', 'max:', indata.max(), '\t', 'avg:', indata.mean(), '\t', 'status:', conversation_status)
+    The output blocks are uninitialized and have to be filled with
+    appropriate audio signals.
+    """
+    assert blocksize != 0
+    q_in = asyncio.Queue()
+    q_out = queue.Queue()
+    loop = asyncio.get_event_loop()
 
-    outdata[:] = indata
+    def callback(indata, outdata, frame_count, time_info, status):
+      loop.call_soon_threadsafe(q_in.put_nowait, (indata.copy(), status))
+      outdata[:] = q_out.get_nowait()
 
+    # pre-fill output queue
+    for _ in range(pre_fill_blocks):
+      q_out.put(np.zeros((blocksize, channels), dtype=dtype))
 
-async def main(**kwargs):
-  print('\nEnough of that, activating wire ...\n')
-  audio_task = asyncio.create_task(wire_coro(**kwargs))
-  await asyncio.sleep(10)
-  audio_task.cancel()
-  try:
-    await audio_task
-  except asyncio.CancelledError:
-    print('\nwire was cancelled')
+    stream = sd.Stream(blocksize=blocksize, callback=callback, dtype=dtype,
+                      channels=channels, **kwargs)
+    with stream:
+      while True:
+        indata, status = await q_in.get()
+        outdata = np.empty((blocksize, channels), dtype=dtype)
+        yield indata, outdata, status
+        q_out.put_nowait(outdata)
+
+  async def wire_coro(self, **kwargs):
+    """
+    Create a connection between audio inputs and outputs.
+
+    Asynchronously iterates over a stream generator and for each block
+    simply copies the input data into the output block.
+    """
+    conversation_status = 'Not Started'
+    last_started_set_time = datetime.now()
+    async for indata, outdata, status in self.stream_generator(**kwargs):
+      if status:
+        self.logger.info(status)
+      if conversation_status == 'Not Started' and indata.max() >= 0.1:
+        self.logger.info(f"Since Conversation Started: {datetime.now() - last_started_set_time}")
+        if (datetime.now() - last_started_set_time) > timedelta(seconds=0.005):
+          conversation_status = 'Started'
+        last_started_set_time = datetime.now()
+      elif conversation_status == 'Started' and indata.max() < 0.1:
+        if (datetime.now() - last_started_set_time) > timedelta(seconds=3):
+          conversation_status = 'Not Started'
+      self.logger.info(f"min:{indata.min()}\tmax:{indata.max()}\tavg:{indata.mean()}\tstatus:{conversation_status}")
+      outdata[:] = indata
+
+  async def main(self, **kwargs):
+    self.logger.info('Enough of that, activating wire ...')
+    audio_task = asyncio.create_task(self.wire_coro(**kwargs))
+    await asyncio.sleep(10)
+    audio_task.cancel()
+    try:
+      await audio_task
+    except asyncio.CancelledError:
+      self.logger.info('wire was cancelled')
 
 
 if __name__ == "__main__":
   try:
-    asyncio.run(main(blocksize=1024))
+    ada = LocalStreamingAda()
+    asyncio.run(ada.main(blocksize=1024))
   except KeyboardInterrupt:
     sys.exit('\nInterrupted by user')
